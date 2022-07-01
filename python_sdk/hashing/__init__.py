36ebac69e02b63febf189efae77248a62709f80c
lijok <lijok@pm.me>
2022-07-01 18:03:55 +0100

    Rename hashing/_hash.py to hashing/_argon2.py

--- python_sdk/hashing/__init__.py
+++ python_sdk/hashing/__init__.py
@@ -1,3 +1,3 @@
-from ._hash import hash_password
-from ._hash import password_matches_hash
-from ._hash import needs_rehash
+from ._argon2 import hash_password
+from ._argon2 import password_matches_hash
+from ._argon2 import needs_rehash
