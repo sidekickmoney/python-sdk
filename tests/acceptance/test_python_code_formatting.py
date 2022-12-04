7e13ed12bae3c00d79c55d1da9796bbe64e323de
sidekick-eimantas <eimantas.gecas@sidekickmoney.com>
2022-12-04 21:54:51 +0000

    Fixup formatting tests

--- tests/acceptance/test_python_code_formatting.py
+++ tests/acceptance/test_python_code_formatting.py
@@ -2,5 +2,19 @@ import subprocess
 
 
 def test_python_code_is_formatted() -> None:
-    result = subprocess.run(["python-sdk", "fmt", "--files", "."])
-    assert result.returncode == 0, result.stdout
+    result = subprocess.run(["black", "--line-length", "120", "--check", "."])
+    assert result.returncode == 0
+    result = subprocess.run(
+        [
+            "isort",
+            "--line-length",
+            "120",
+            "--force-single-line-imports",
+            "--force-sort-within-sections",
+            "--profile",
+            "black",
+            "--check",
+            ".",
+        ]
+    )
+    assert result.returncode == 0
