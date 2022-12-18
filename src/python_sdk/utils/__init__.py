6e9e1b47555fcc0ad458e808589478a66474aa15
sidekick-eimantas <eimantas.gecas@sidekickmoney.com>
2022-12-18 16:25:29 +0000

    Add ansi utils

--- src/python_sdk/utils/__init__.py
+++ src/python_sdk/utils/__init__.py
@@ -1,3 +1,4 @@
+from python_sdk.utils._ansi import remove_ansi_escape_sequences as remove_ansi_escape_sequences
 from python_sdk.utils._checksum import get_file_sha1 as get_file_sha1
 from python_sdk.utils._connectivity import is_connected as is_connected
 from python_sdk.utils._file_watcher import FileStats as FileStats
---
+++ src/python_sdk/utils/_ansi.py
@@ -0,0 +1,6 @@
+import re
+
+
+def remove_ansi_escape_sequences(string: str) -> str:
+    ansi_escape = re.compile(r"\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])")
+    return ansi_escape.sub("", string)
