c819a0817b0a832524a0311ea64918b78a9cd6de
sidekick-eimantas <eimantas.gecas@sidekickmoney.com>
2022-12-18 16:21:48 +0000

    Add connectivity utils

--- src/python_sdk/utils/__init__.py
+++ src/python_sdk/utils/__init__.py
@@ -1,3 +1,4 @@
 from python_sdk.utils._flag import BoolFlag as BoolFlag
 from python_sdk.utils._templates import render_template as render_template
 from python_sdk.utils._zipfile import ZipFileWithPermissions as ZipFileWithPermissions
+from python_sdk.utils._connectivity import is_connected as is_connected
