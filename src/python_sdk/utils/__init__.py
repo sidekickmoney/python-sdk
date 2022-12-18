b0657dbc8e8b12879535b300dae3142145e0fbaa
sidekick-eimantas <eimantas.gecas@sidekickmoney.com>
2022-12-18 16:23:53 +0000

    Add file watcher utils

--- src/python_sdk/utils/__init__.py
+++ src/python_sdk/utils/__init__.py
@@ -1,4 +1,6 @@
+from python_sdk.utils._connectivity import is_connected as is_connected
+from python_sdk.utils._file_watcher import FileStats as FileStats
+from python_sdk.utils._file_watcher import file_watcher as file_watcher
 from python_sdk.utils._flag import BoolFlag as BoolFlag
 from python_sdk.utils._templates import render_template as render_template
 from python_sdk.utils._zipfile import ZipFileWithPermissions as ZipFileWithPermissions
-from python_sdk.utils._connectivity import is_connected as is_connected
