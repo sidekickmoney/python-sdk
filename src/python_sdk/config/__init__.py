897a70d756d432682e89b98ae3ec0a6f8a216a30
sidekick-eimantas <eimantas.gecas@sidekickmoney.com>
2022-12-23 22:05:40 +0000

    Fix config type hints

--- src/python_sdk/config/__init__.py
+++ src/python_sdk/config/__init__.py
@@ -1,4 +1,3 @@
-from ._config import _ConfigProtocol as _ConfigProtocol
 from ._config import config as config
 from ._config_decoder import Base64EncodedString as Base64EncodedString
 from ._config_decoder import UnvalidatedDict as UnvalidatedDict
--- src/python_sdk/config/_config.py
+++ src/python_sdk/config/_config.py
@@ -22,6 +22,8 @@ class _LocalFile:
     filepath: typing.Union[str, pathlib.Path]
 
 
+# TODO: aws-parameter-store document
+# TODO: aws-secrets-manager-store document
 _SOURCE_FROM_OPTIONS = ["ENVIRONMENT_VARIABLES", "LOCAL_FILE"]
 
 
@@ -51,61 +53,51 @@ def _get_config_source() -> typing.Union[_EnvironmentVariables, _LocalFile]:
     return source_from
 
 
-class _ConfigProtocol(typing.Protocol):
-    _PREFIX: str
-    _SOURCE_FROM: typing.Union[_EnvironmentVariables, _LocalFile]
+_T = typing.TypeVar("_T")
 
 
-def config(
-    cls: typing.Type = None,
-    /,
-    *,
-    prefix: str,
-) -> typing.Callable[[typing.Type], _ConfigProtocol]:
-    def _wrap(cls: typing.Type) -> _ConfigProtocol:
+@dataclasses.dataclass
+class _ConfigClassMeta:
+    """Container for config class metadata."""
+
+    prefix: str
+    source_from: _SOURCE_FROM_OPTIONS
+
+
+def config(*, prefix: str) -> typing.Callable[[_T], _T]:
+    def _wrap(cls: _T) -> _T:
         return _process_config_class(cls=cls, prefix=prefix)
 
     return _wrap
 
 
-def _process_config_class(
-    cls: typing.Type,
-    prefix: str,
-) -> _ConfigProtocol:
-    cls._PREFIX = prefix
+def _process_config_class(cls: _T, prefix: str) -> _T:
     try:
-        cls._SOURCE_FROM = _get_config_source()
-        cls = typing.cast(_ConfigProtocol, cls)
-        _enforce_upper_case_conventions(cls=cls)
+        cls._meta = _ConfigClassMeta(prefix=prefix, source_from=_get_config_source())
+
+        _enforce_upper_case_prefix(cls=cls)
+        _enforce_upper_case_config_options(cls=cls)
         _enforce_config_options_types(cls=cls)
         _load_config(cls=cls)
     except Exception as e:
         _log.exception(e)
         raise
+
     return cls
 
 
-def _enforce_upper_case_conventions(cls: _ConfigProtocol) -> None:
-    # the point of the library is to enforce consistency among projects
-    # therefore, we enforce that prefix and config options are all uppercase
-    # which is not to say all uppercase is better than all lowercase
-    # but a choice had to be made
-    _enforce_upper_case_prefix(cls=cls)
-    _enforce_upper_case_config_options(cls=cls)
-
-
-def _enforce_upper_case_prefix(cls: _ConfigProtocol) -> None:
-    if cls._PREFIX.upper() != cls._PREFIX:
+def _enforce_upper_case_prefix(cls: _T) -> None:
+    if cls._meta.prefix.upper() != cls._meta.prefix:
         raise ValueError("Prefix must be uppercase to ensure consistency")
 
 
-def _enforce_upper_case_config_options(cls: _ConfigProtocol) -> None:
+def _enforce_upper_case_config_options(cls: _T) -> None:
     for key in cls.__annotations__.keys():
         if key.upper() != key:
             raise ValueError("All config options must be uppercase to ensure consistency")
 
 
-def _enforce_config_options_types(cls: _ConfigProtocol) -> None:
+def _enforce_config_options_types(cls: _T) -> None:
     for config_option, data_type in cls.__annotations__.items():
         try:
             type_is_supported = _config_decoder.type_is_supported(data_type=data_type)
@@ -115,21 +107,21 @@ def _enforce_config_options_types(cls: _ConfigProtocol) -> None:
             raise TypeError(f"Config option {config_option} type {data_type} not supported")
 
 
-def _load_config(cls: _ConfigProtocol) -> _ConfigProtocol:
+def _load_config(cls: _T) -> _T:
     unprocessed_config = _get_config(cls=cls)
     processed_config = _process_config(cls=cls, unprocessed_config=unprocessed_config)
     _apply_config_to_config_class(cls=cls, config=processed_config)
     return cls
 
 
-def _get_config(cls: _ConfigProtocol) -> typing.Dict[str, str]:
-    if isinstance(cls._SOURCE_FROM, _EnvironmentVariables):
-        return _get_config_from_environment_variables(prefix=cls._PREFIX)
-    elif isinstance(cls._SOURCE_FROM, _LocalFile):
-        return _get_config_from_local_file(prefix=cls._PREFIX, filepath=cls._SOURCE_FROM.filepath)
+def _get_config(cls: _T) -> typing.Dict[str, str]:
+    if isinstance(cls._meta.source_from, _EnvironmentVariables):
+        return _get_config_from_environment_variables(prefix=cls._meta.prefix)
+    elif isinstance(cls._meta.source_from, _LocalFile):
+        return _get_config_from_local_file(prefix=cls._meta.prefix, filepath=cls._meta.source_from.filepath)
     else:
         # this should never happen
-        raise AssertionError(f"Loading config from: {cls._SOURCE_FROM} is not supported.")
+        raise AssertionError(f"Loading config from: {cls._meta.source_from} is not supported.")
 
 
 def _get_config_from_environment_variables(prefix: str) -> typing.Dict[str, str]:
@@ -139,10 +131,10 @@ def _get_config_from_environment_variables(prefix: str) -> typing.Dict[str, str]
 def _get_config_from_local_file(prefix: str, filepath: str) -> typing.Dict[str, str]:
     try:
         config_document = pathlib.Path(filepath).read_text()
-    except FileNotFoundError:
-        raise FileNotFoundError(f"File not found: {filepath}")
-    except PermissionError:
-        raise PermissionError(f"Encountered a permission error when trying to read file: {filepath}")
+    except FileNotFoundError as e:
+        raise FileNotFoundError(f"File not found: {filepath}") from e
+    except PermissionError as e:
+        raise PermissionError(f"Encountered a permission error when trying to read file: {filepath}") from e
 
     return _parse_config_document(prefix=prefix, config_document=config_document)
 
@@ -157,10 +149,10 @@ def _parse_config_document(prefix: str, config_document: str) -> typing.Dict[str
     return config
 
 
-def _process_config(cls: _ConfigProtocol, unprocessed_config: typing.Dict[str, str]) -> typing.Dict[str, typing.Any]:
+def _process_config(cls: _T, unprocessed_config: typing.Dict[str, str]) -> typing.Dict[str, typing.Any]:
     processed_config = {}
     for key, unprocessed_value in unprocessed_config.items():
-        config_option = key.split(cls._PREFIX, 1)[1]
+        config_option = key.split(cls._meta.prefix, 1)[1]
         if not config_option:
             _log.warning(f"Found empty config option key: {key}")
             continue
@@ -176,7 +168,7 @@ def _process_config(cls: _ConfigProtocol, unprocessed_config: typing.Dict[str, s
     return processed_config
 
 
-def _process_config_value(cls: _ConfigProtocol, config_option: str, unprocessed_value: str) -> typing.Any:
+def _process_config_value(cls: _T, config_option: str, unprocessed_value: str) -> typing.Any:
     config_option_data_type = cls.__annotations__[config_option]
     try:
         processed_value = _config_decoder.decode_config_value(
@@ -184,14 +176,14 @@ def _process_config_value(cls: _ConfigProtocol, config_option: str, unprocessed_
         )
     except ValueError as e:
         raise ValueError(
-            f"Could not cast config option {cls._PREFIX}{config_option} with value {unprocessed_value} to datatype "
+            f"Could not cast config option {cls._meta.prefix}{config_option} with value {unprocessed_value} to datatype "
             f"{config_option_data_type}",
         ) from e
 
     return processed_value
 
 
-def _apply_config_to_config_class(cls: _ConfigProtocol, config: typing.Dict[str, str]) -> None:
+def _apply_config_to_config_class(cls: _T, config: typing.Dict[str, str]) -> None:
     unset = object()
     for config_option, data_type in cls.__annotations__.items():
         default = getattr(cls, config_option, unset)
@@ -205,4 +197,4 @@ def _apply_config_to_config_class(cls: _ConfigProtocol, config: typing.Dict[str,
         elif config_option_is_optional:
             setattr(cls, config_option, None)
         else:
-            raise ValueError(f"Required config option {cls._PREFIX}{config_option} with no default was not set")
+            raise ValueError(f"Required config option {cls._meta.prefix}{config_option} with no default was not set")
