24bce41c7650b05092a6fa2d587b210fccd34152
sidekick-eimantas <eimantas.gecas@sidekickmoney.com>
2023-02-25 12:39:04 +0000

    Add tests that test yanking context values from messages

---
+++ tests/functional/log/test_formatters.py
@@ -0,0 +1,25 @@
+import json
+
+import pytest
+
+from python_sdk import log
+
+
+def test_context_embedded_in_log_message_is_yanked(capsys: pytest.CaptureFixture) -> None:
+    log.Log.configure_logging()
+    user_id = 123
+    log.info(f"test {user_id=}")
+    captured_log = json.loads(capsys.readouterr().out)
+    assert captured_log["message"] == "test"
+    assert captured_log["user_id"] == str(user_id)
+
+
+def test_multiple_context_values_embedded_in_log_message_are_yanked(capsys: pytest.CaptureFixture) -> None:
+    log.Log.configure_logging()
+    user_id = 123
+    user_name = "test"
+    log.info(f"test {user_id=} {user_name=}")
+    captured_log = json.loads(capsys.readouterr().out)
+    assert captured_log["message"] == "test"
+    assert captured_log["user_id"] == str(user_id)
+    assert captured_log["user_name"] == user_name
